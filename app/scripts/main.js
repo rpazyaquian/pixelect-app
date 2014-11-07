91fb467c5e6fd6f2e4ddfa6158b4ab1c4d7e7c05
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-07 16:43:24 -0500

    Added backbone and handlebars to bower.json dependencies. Ran bower isntall. Added Backbone routes to the main.js file.
    added   var router = new Router();
      Backbone.history.start(); to the end of main.js file.
    
    Trying to push
    
    ran bower install, grunt
    
    Added routes, created a test-file under root-app-directory: main.html; now woring on show-image route.
    
    Am experimenting with html and routes. Need to install backbone.js.
    
    Erased changes I made beyond simply adding backbone routes.
    
    Added backbone and handlebars to bower.json dependencies. ran bower isntall.
    
    Pulling
    
    Added   var router = new Router();
      Backbone.history.start(); to main.js.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+var Router = Backbone.Router.extend({
+    routes:{
+
+      '': "home",
+      'sign_up': "sign_up",
+      'sign_in': "sign_in",
+      'upload': "upload",
+      'about': "about",
+      'show_album' :"show_album"
+    },
+
+});
+
+
+
+  var router = new Router();
+  Backbone.history.start();
