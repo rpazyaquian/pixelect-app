d8e2cbd08aa1f818fa83c98f829c61ffae8dc16a
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-07 16:43:24 -0500

    Added backbone and handlebars to bower.json dependencies. Ran bower isntall. Added Backbone routes to the main.js file.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+var Router = Backbone.Router.extend({
+    routes:{
+
+      '': "home",
+      'sign_up': "sign_up",
+      'sign_in': "sign_in",
+      'upload': "upload",
+      'about': "about",
+      'show_album' :"show_album"
+    },
+
+});
