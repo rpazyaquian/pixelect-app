09eed336846bafca37333b71d985f9eb4f192485
cxoltero <carlosh95@gmail.com>
2014-11-10 11:41:25 -0500

    fixed the scope for bower components

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+var Router = Backbone.Router.extend({
+    routes:{
+
+      '': "home",
+      'home': "home",
+      'sign_up': "sign_up",
+      'sign_in': "sign_in",
+      'upload': "upload",
+      'about': "about",
+      'show_album' :"show_album"
+    },
+
+// ###################################################
+// UPLOAD
+
+upload: function(){
+  alert('jsworking');
+  $('#container').empty();
+  console.log('it is working!');
+},
+
+
+// ######################################################
+});
+
+
+// end of backbone
+// #####################################################
+
+var router = new Router();
+Backbone.history.start();
