c7200aab9bd7969dcf17f762b0108d31e7de9914
CollinBlinder <collinblinder@gmail.com>
2014-11-11 11:02:19 -0500

    tried to resolve merge conflicts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,10 +18,8 @@ var Router = Backbone.Router.extend({
     }
 
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 6b39cbf555bca93c39d69a51bda18bf3c90bb0da
+
+
 // ###################################################
 // UPLOAD
 
@@ -39,18 +37,10 @@ upload: function(){
 // end of backbone
 // #####################################################
 
-var router = new Router();
-Backbone.history.start();
-<<<<<<< HEAD
-=======
->>>>>>> 54923f4b3e9c16d1e970cd5d34bdebd34f3fc706
-});
+
 
 
 
 
   var router = new Router();
   Backbone.history.start();
->>>>>>> 2e68b8044d4839b08e4c1b86c2653bc753642139
-=======
->>>>>>> 6b39cbf555bca93c39d69a51bda18bf3c90bb0da
