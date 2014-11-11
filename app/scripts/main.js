51976a45b0a03ebf7b871efb59202fbb7083033e
cxoltero <carlosh95@gmail.com>
2014-11-10 21:27:26 -0500

    added files for amazon s3, upload not working yet

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,55 @@
-var Router = Backbone.Router.extend({
-    routes:{
+// var Router = Backbone.Router.extend({
+//     routes:{
 
-      '': "home",
-      'home': "home",
-      'sign_up': "sign_up",
-      'sign_in': "sign_in",
-      'upload': "upload",
-      'about': "about",
-      'show_album' :"show_album"
-    },
+//       '': "home",
+//       'home': "home",
+//       'sign_up': "sign_up",
+//       'sign_in': "sign_in",
+//       'upload': "upload",
+//       'about': "about",
+//       'show_album' :"show_album"
+//     },
+
+
+
+
+    var ImageApp = ImageApp || {}
+
+ImageApp.getAmazonURL = function() {
+  $.ajax({
+    url: 'http://localhost:3000/amazon/sign_key',
+    type: 'GET',
+    data: {file_name: 'url.jpg'},
+  })
+  .done(function(result) {
+    $('#uploadPolicy').val(result.policy);
+    $('#uploadSignature').val(result.signature);
+    $('#accessKey').val(result.access_key);
+    $('#acl').val(result.acl);
+    $('#key').val(result.key);
+  })
+  .fail(function(error) {
+    console.log(error);
+    console.log('error');
+  })
+  .always(function() {
+    console.log("complete");
+  });
+};
+
+
+$(document).ready(function() {
+  ImageApp.getAmazonURL();
+});
 
 // ###################################################
 // UPLOAD
 
-upload: function(){
-  alert('jsworking');
-  $('#container').empty();
-  console.log('it is working!');
-},
+// upload: function(){
+//   alert('jsworking');
+//   $('#container').empty();
+//   console.log('it is working!');
+// },
 
 
 // ######################################################
@@ -27,5 +59,5 @@ upload: function(){
 // end of backbone
 // #####################################################
 
-var router = new Router();
-Backbone.history.start();
+// var router = new Router();
+// Backbone.history.start();
