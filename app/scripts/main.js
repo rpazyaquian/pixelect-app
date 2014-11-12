223738b64cd994fb364a33a64ca01b912eb154ca
CollinBlinder <collinblinder@gmail.com>
2014-11-12 09:21:48 -0500

    Replaced local url in the ticket-get ajax request with our heroku address.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -92,31 +92,31 @@ upload: function(){
 
 
 
-// var ImageApp = ImageApp || {}
+var ImageApp = ImageApp || {}
 
-// ImageApp.getAmazonURL = function() {
-//   $.ajax({
-//     url: 'http://localhost:3000/amazon/sign_key',
-//     type: 'GET',
-//     data: {file_name: 'url.jpg'},
-//   })
-//   .done(function(result) {
-//     $('#uploadPolicy').val(result.policy);
-//     $('#uploadSignature').val(result.signature);
-//     $('#accessKey').val(result.access_key);
-//     $('#acl').val(result.acl);
-//     $('#key').val(result.key);
-//   })
-//   .fail(function(error) {
-//     console.log(error);
-//     console.log('error');
-//   })
-//   .always(function() {
-//     console.log("complete");
-//   });
-// };
+ImageApp.getAmazonURL = function() {
+  $.ajax({
+    url: 'https://polar-chamber-4218.herokuapp.com/amazon/sign_key',
+    type: 'GET',
+    data: {file_name: 'url.jpg'},
+  })
+  .done(function(result) {
+    $('#uploadPolicy').val(result.policy);
+    $('#uploadSignature').val(result.signature);
+    $('#accessKey').val(result.access_key);
+    $('#acl').val(result.acl);
+    $('#key').val(result.key);
+  })
+  .fail(function(error) {
+    console.log(error);
+    console.log('error');
+  })
+  .always(function() {
+    console.log("complete");
+  });
+};
 
 
-// $(document).ready(function() {
-//   ImageApp.getAmazonURL();
-// });
+$(document).ready(function() {
+  ImageApp.getAmazonURL();
+});
