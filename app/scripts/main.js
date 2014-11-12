8c06e54e12f2cff74bd2708e8e851cf43d7a3ead
CollinBlinder <collinblinder@gmail.com>
2014-11-12 09:12:33 -0500

    About to work on merge conflicts.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
 var Router = Backbone.Router.extend({
-    routes:{
-      '': "home",
-      'home': "home",
-      'sign_up': "sign_up",
-      'sign_in': "sign_in",
-      'upload': "upload",
-      'about': "about",
-      'show_album' :"show_album"
+    routes: {
+      '': 'home',
+      'home': 'home',
+      'sign_up': 'sign_up',
+      'sign_in': 'sign_in',
+      'upload': 'upload',
+      'about': 'about',
+      'show_album' :'show_album'
     },
 
     // initialize: function(){
@@ -19,12 +19,13 @@ var Router = Backbone.Router.extend({
 
 
     sign_in: function(){
+      "use strict";
       var template = Handlebars.compile($("#sign_inTemplate").html());
-        $('#content').html(template({
+        $('#handlebarsContainer').html(template({
 
         }));
 
-    }
+    },
     // show_album: function(){
     //   console.log($('.jumbotron > h1').text())
 
@@ -42,37 +43,35 @@ var Router = Backbone.Router.extend({
 // ###################################################
 // UPLOAD
 
-// upload: function(){
-//   "use strict";
-//   // $('#handlebarsContainer').empty();
-//   var template = Handlebars.compile($('#uploadTemplate').html());
-//     $('#handlebarsContainer').html(template({
+upload: function(){
+  "use strict";
+  // $('#handlebarsContainer').empty();
+  var template = Handlebars.compile($('#uploadTemplate').html());
+    $('#handlebarsContainer').html(template({
 
 
-//     }));
-//     // console.log($('input[name="uploadedImage"]').val())
-//     console.log($('#upImg').val())
-//     $('#submitupImg').on('click', function(){
-//       console.log($('#upImg').val())
-//         var imageSet = {
-//               image: {
-//                 file: $('#upImg').val()
-//               }
-//         }
+    }));
+    // console.log($('input[name="uploadedImage"]').val())
+    console.log($('#upImg1').val())
+    $('#submitupImg').on('click', function(){
+      console.log($('#upImg1').val())
+        var imageSet = {
+              image: {
+                file: $('#upImg1').val()
+              }
+        }
+    });
 
-
-//     });
-
-//     $.ajax({
-//         url: 'https://polar-chamber-4218.herokuapp.com/image_sets',
-//         type: 'POST',
-//         data:data
-//       })
-//   // alert("upload worked")
+    // $.ajax({
+    //     url: 'https://polar-chamber-4218.herokuapp.com/image_sets',
+    //     type: 'POST',
+    //     data:data
+    //   })
+    //   alert("upload worked")
 
 
 
-// }
+}
 
 
 // ######################################################
@@ -93,61 +92,61 @@ var Router = Backbone.Router.extend({
 
 
 
-var ImageApp = ImageApp || {}
+// var ImageApp = ImageApp || {}
 
-ImageApp.getAmazonURL = function() {
-  $.ajax({
-    url: 'http://localhost:3000/amazon/sign_key',
-    type: 'GET',
-    data: {file_name: 'url.jpg'},
-  })
-  .done(function(result) {
-    $('#uploadPolicy').val(result.policy);
-    $('#uploadSignature').val(result.signature);
-    $('#accessKey').val(result.access_key);
-    $('#acl').val(result.acl);
-    $('#key').val(result.key);
-  })
-  .fail(function(error) {
-    console.log(error);
-    console.log('error');
-  })
-  .always(function() {
-    console.log("complete");
-  });
-};
-
-
-$(document).ready(function() {
-  ImageApp.getAmazonURL();
-});
-=======
-var ImageApp = ImageApp || {}
-
-ImageApp.getAmazonURL = function() {
-  $.ajax({
-    url: 'http://localhost:3000/amazon/sign_key',
-    type: 'GET',
-    data: {file_name: 'url.jpg'},
-  })
-  .done(function(result) {
-    $('#uploadPolicy').val(result.policy);
-    $('#uploadSignature').val(result.signature);
-    $('#accessKey').val(result.access_key);
-    $('#acl').val(result.acl);
-    $('#key').val(result.key);
-  })
-  .fail(function(error) {
-    console.log(error);
-    console.log('error');
-  })
-  .always(function() {
-    console.log("complete");
-  });
-};
+// ImageApp.getAmazonURL = function() {
+//   $.ajax({
+//     url: 'http://localhost:3000/amazon/sign_key',
+//     type: 'GET',
+//     data: {file_name: 'url.jpg'},
+//   })
+//   .done(function(result) {
+//     $('#uploadPolicy').val(result.policy);
+//     $('#uploadSignature').val(result.signature);
+//     $('#accessKey').val(result.access_key);
+//     $('#acl').val(result.acl);
+//     $('#key').val(result.key);
+//   })
+//   .fail(function(error) {
+//     console.log(error);
+//     console.log('error');
+//   })
+//   .always(function() {
+//     console.log("complete");
+//   });
+// };
 
 
-$(document).ready(function() {
-  ImageApp.getAmazonURL();
-});
->>>>>>> fc1d71e5d29353395b55a97c0168069b3d290ea9
+// $(document).ready(function() {
+//   ImageApp.getAmazonURL();
+// });
+// =======
+// var ImageApp = ImageApp || {}
+
+// ImageApp.getAmazonURL = function() {
+//   $.ajax({
+//     url: 'http://localhost:3000/amazon/sign_key',
+//     type: 'GET',
+//     data: {file_name: 'url.jpg'},
+//   })
+//   .done(function(result) {
+//     $('#uploadPolicy').val(result.policy);
+//     $('#uploadSignature').val(result.signature);
+//     $('#accessKey').val(result.access_key);
+//     $('#acl').val(result.acl);
+//     $('#key').val(result.key);
+//   })
+//   .fail(function(error) {
+//     console.log(error);
+//     console.log('error');
+//   })
+//   .always(function() {
+//     console.log("complete");
+//   });
+// };
+
+
+// $(document).ready(function() {
+//   ImageApp.getAmazonURL();
+// });
+// >>>>>>> fc1d71e5d29353395b55a97c0168069b3d290ea9
